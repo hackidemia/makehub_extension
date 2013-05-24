0c2ec3bcef71cbe4fd67abb5f73f0e87acf19df1
Allard van Helbergen <allard@vanhelbergen.com>
2013-05-25 01:53:51 +0200

    encapsulation

--- popup.js
+++ popup.js
@@ -1,6 +1,14 @@
-console.log(chrome.tabs);
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      alert(tabs[0].url);
-   }
-);
+var hck = {};
+
+
+hck.getUrl = function() {
+  chrome.tabs.query({
+    'active': true,
+    'windowId': chrome.windows.WINDOW_ID_CURRENT
+  }, function(tabs) {
+    alert(tabs[0].url);
+  });
+};
+
+
+hck.getUrl();
