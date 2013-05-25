8f98b826c80b0752628c9c5e1627f0e19939b8f3
Stefania Druga <druga.stefania@gmail.com>
2013-05-25 15:01:00 +0200

    modifications to extension text and styles

--- popup.js
+++ popup.js
@@ -8,7 +8,19 @@ hck.getUrl = function() {
   }, function(tabs) {
     //alert(tabs[0].url);
   });
+
 };
 
 
 hck.getUrl();
+
+document.addEventListener('mouseup',function(event)
+{
+  var sel = window.getSelection().toString();
+   alert(sel);
+  if(sel.length)
+    chrome.extension.sendRequest({'message':'setText','data': sel},function(response){
+
+    })
+
+})
